f6b983204b3d57cd83bdbe0b4b6fe87d2c98f476
Samuel Kimathi Mithika <KimathiWebsites@gmail.com>
2025-02-16 13:23:51 +0300

    Program2.php

---
+++ Program2.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Program 2</title>
+    </head>
+    <body>
+        <?php
+            ECHO "2022 Corona Virus is not so bad........!<br>";
+            echo "Valentine was not Affected...!<br>";
+            ECHO "I Went on a date et Kikuyu Gardens at Club Desire Where Gedeo and Beatrice Were....!<br>";
+
+        ?>
+    </body>
+
+</html>

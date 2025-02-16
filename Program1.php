58e85d8b329e77b9db1c181cdd721e65dbe7a703
Samuel Kimathi Mithika <KimathiWebsites@gmail.com>
2025-02-16 13:23:26 +0300

    Commiting Program1.php

---
+++ Program1.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>This is the Title bar to appear at the Top<title>
+    </head>
+    <body>
+        <?php "My First Program in PHP"; ?>
+    </body>
+</html>

4546e3892420fb771b40526573a0c3ab2dc4947f
Samuel Kimathi Mithika <KimathiWebsites@gmail.com>
2025-02-16 13:25:50 +0300

    Commiting Program4.php

---
+++ Program4.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Program 4</title>
+    </head>
+    <body>
+        <?php
+        $txt1 = "Learning PHP";
+        $txt2 = "Presbyterian University in Kiambu County";
+        $x = 4;
+        $y = 5;
+        echo "<h2>".$txt1."</h2>";//Adding a heading to text...//
+        echo "We are studying PHP at ".$txt2."<br>";
+        echo $x + $y;
+        ?>
+    </body>
+</html>

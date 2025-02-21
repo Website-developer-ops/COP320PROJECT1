49cc23f7faf4dcc9d504d3588f264cff04b23871
Major General Robert Kibochi <CaptainIbrahimTraore@gmail.com>
2025-02-21 14:52:50 +0300

    Deleted Program1 - 4 Files

--- Program1.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>This is the Title bar to appear at the Top<title>
-    </head>
-    <body>
-        <?php "My First Program in PHP"; ?>
-    </body>
-</html>

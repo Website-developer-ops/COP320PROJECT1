3e12aef1c4c530300f392778d5a97ec1afce73d3
Major General Robert Kibochi <CaptainIbrahimTraore@gmail.com>
2025-02-21 14:23:45 +0300

    Commiting Program2.php

---
+++ PHP/Program2.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Program 2</title>
+        <Link rel="Stylesheet" href="../CSS/Program2.css">
+    </head>
+    <body>
+        <?php
+            ECHO "<p class='p1'>2022 Corona Virus is not so bad........!</p>";
+            echo "<p class='p2'>Valentine was not Affected...!<br></p>";
+            ECHO "<p class= 'p3'>I Went on a date et Kikuyu Gardens at Club Desire Where Gedeo and Beatrice Were....!<br></p>";
+
+        ?>
+    </body>
+
+</html>

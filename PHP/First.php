9f1b3494a1eae59251e609a0b97eb87a34b4bbc8
Samuel Kimathi Mithika <KimathiWebsites@gmail.com>
2025-02-21 15:09:00 +0300

    Commiting First.php

---
+++ PHP/First.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <?php
+// This is a simple PHP program that outputs "Hello, World!"
+        echo "Hello, World!";
+        phpinfo();
+        ?>
+
+    </body>
+</html>

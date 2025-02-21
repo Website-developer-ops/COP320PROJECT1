7f12084435508bb12f39a3280580d93de8e477af
Samuel Kimathi Mithika <KimathiWebsites@gmail.com>
2025-02-21 15:08:07 +0300

    Commiting Program7.php

---
+++ PHP/Program7.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <?php
+            $age = array("Peter" => "35", "Ben" => "37", "Joe" => "43");
+            echo "Peter is: ".$age['Peter']. " years old.";
+        ?>
+    </body>
+<html>

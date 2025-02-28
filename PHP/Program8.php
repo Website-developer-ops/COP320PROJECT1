3cb801e694eeb5431db4191d5c7fa6e5bc0613f8
Samuel Kimathi Mithika <KimathiWebsites@gmail.com>
2025-02-28 14:20:35 +0300

    Commiting Program8.php

---
+++ PHP/Program8.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PROGRAM 8</title>
+    </head>
+    <body>
+        <form>
+            <label>Name: </label><input type="text" name="name"><br>
+            <label>E-Email: </label><input type="text" name="email"><br>
+            <button>Submit</button>
+        </form>
+    </body>
+</html>
